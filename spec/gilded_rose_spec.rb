f50871e992b0f390b8cf8e61cb72ecc63fc5db67
Naz <nazmalik08@gmail.com>
2017-05-16 11:42:10 +0100

    Add tests for passes

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -5,7 +5,8 @@ describe GildedRose do
   let(:vest) { Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)}
   let(:brie) { Item.new(name="Aged Brie", sell_in=2, quality=0)}
   let(:sulfuras) { Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80) }
-  let(:items) {[vest, brie, sulfuras]}
+  let(:passes) { Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20) }
+  let(:items) {[vest, brie, sulfuras, passes]}
 
   subject(:gildedrose) { described_class.new(items)}
 
@@ -44,6 +45,27 @@ describe GildedRose do
       100.times{ gildedrose.update_quality }
       expect(sulfuras.sell_in).to eq 0
     end
+
+    it "Backstage passes increase in quality as sell_in approaches 0" do
+      expect(passes.quality).to eq 21
+    end
+
+    it "Backstage passes increase in quality by 2 when sell_in <= 10" do
+      5.times { gildedrose.update_quality }
+      expect(passes.quality).to eq 27
+    end
+
+    it "Backstage passes increase in quality by 3 when sell_in <= 5" do
+      10.times { gildedrose.update_quality }
+      expect(passes.sell_in).to eq 4
+      expect(passes.quality).to eq 38
+    end
+
+    it "Backstage passes increase in quality by 2 when sell_in <= 10" do
+      15.times { gildedrose.update_quality }
+      expect(passes.quality).to eq 0
+    end
+
   end
 
 end
