aed673e5a37d5c10c4d3a9f0daaa05c284f7a20a
Naz <nazmalik08@gmail.com>
2017-05-16 12:01:30 +0100

    Refactor ruby syntax

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,49 +6,48 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+
+      #normal items
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          item.quality -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
         end
+
+      #Brie and passes
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          item.quality += 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality += 1 if item.quality < 50
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality += 1 if item.quality < 50
             end
           end
         end
       end
+
+      #Sulfuras
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
       end
+
+      #normal items
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
+              item.quality -= 1 if item.name != "Sulfuras, Hand of Ragnaros"
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality += 1 if item.quality < 50
         end
       end
+
     end
   end
 end
