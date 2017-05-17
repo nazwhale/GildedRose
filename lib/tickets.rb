bd89635f75e40ad67a66a472a46c15e49d67a517
Naz <nazmalik08@gmail.com>
2017-05-17 18:30:26 +0100

    Update max quality for backstage passes

--- lib/tickets.rb
+++ lib/tickets.rb
@@ -12,9 +12,12 @@ class Tickets < Item
 
   def update
     self.passage_of_time
-    self.improve
-    self.improve if sell_in <= 10
-    self.improve if sell_in <= 5
+    if quality < 50
+      self.improve
+      self.improve if sell_in <= 10
+      self.improve if sell_in <= 5
+    end
+    self.quality = 50 if quality > 50
     self.quality = 0 if sell_in <= 0
   end
 
